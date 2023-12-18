0d782b11b707ecbba58bd715895bd7b099370bb6
vuduyhn96 <vuanhduyhn1996@gmail.com>
2023-12-18 13:12:25 +0700

    car1

---
+++ Car.java
@@ -0,0 +1,10 @@
+public class Car {
+    private String name;
+    private String engine;
+    public static int numberOfCars;
+    public Car(String name, String engine) {
+        this.name = name;
+        this.engine = engine;
+        numberOfCars++;
+    }
+}
